961f45b5dac66d88e55f4f73dbec554fe3ca4df9
xuan45 <315764194@qq.com>
2019-05-31 12:31:13 +0800

    feat: enable gzip encoding

--- vis-server/app.js
+++ vis-server/app.js
@@ -1,44 +1,50 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const cors = require('koa2-cors')
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const cors = require('koa2-cors');
+const compress = require('koa-compress');
 
-const index = require('./routes/index')
+const index = require('./routes/index');
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(cors())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(cors());
+app.use(compress());
+app.use(require('koa-static')(__dirname + '/public'));
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + '/views', {
+    extension: 'pug',
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
-app.use(index.routes(), index.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
-module.exports = app
+module.exports = app;
--- vis-server/routes/index.js
+++ vis-server/routes/index.js
@@ -1,65 +1,95 @@
-const router = require('koa-router')()
-const knex = require('../knex')
+const router = require('koa-router')();
+const knex = require('../knex');
 
 router.get('/day1', async (ctx, next) => {
   let back = {
-    message:'fail',
-    data:{
-      time:[],
-      count:[]
-    }
-  }
-  await knex('day1_PertimeSum').select('*').orderBy('time','asc').then(e=>{
-    let time = []
-    let count = []
-    for(let o of e){
-      time.push(o.time)
-      count.push(o.count)
-    }
-    back.data.time = time
-    back.data.count = count
-    back.message = 'success'
-  })
-  ctx.body = back
-})
+    message: 'fail',
+    data: {
+      time: [],
+      count: [],
+    },
+  };
+  await knex('day1_PertimeSum')
+    .select('*')
+    .orderBy('time', 'asc')
+    .then(e => {
+      let time = [];
+      let count = [];
+      for (let o of e) {
+        time.push(o.time);
+        count.push(o.count);
+      }
+      back.data.time = time;
+      back.data.count = count;
+      back.message = 'success';
+    });
+  ctx.body = back;
+});
 
-router.get('/day1func',async (ctx,next)=>{
-  let back={
-    message:'fail',
-    data:{
-      time:[],
-      count:[]
-    }
-  }
-  let func = ctx.query.function
-  await knex('day1_PertimeFunction').select('time','count').where('function',func).orderBy('time','asc').then(e=>{
-    let time = []
-    let count = []
-    for(let o of e){
-      time.push(o.time)
-      count.push(o.count)
-    }
-    back.data.time = time
-    back.data.count = count
-    back.message = 'success'
-  })
-  ctx.body = back
-})
-
-router.get('/heatMap',async(ctx,next)=>{
+router.get('/day1func', async (ctx, next) => {
   let back = {
-    message:'fail',
-    data:[]
-  }
-  let floor = ctx.query.floor
-  await knex('day1_PertimeSid').join('sensor','day1_PertimeSid.sid','=','sensor.sid').where('floor',floor).select('time','x','y','count','floor').then(e=>{
-    if(e.length === 0) return;
-    else{
-      back.message = 'success'
-      back.data = e
-    }
-  })
-  ctx.body = back
-})
+    message: 'fail',
+    data: {
+      time: [],
+      count: [],
+    },
+  };
+  let func = ctx.query.function;
+  await knex('day1_PertimeFunction')
+    .select('time', 'count')
+    .where('function', func)
+    .orderBy('time', 'asc')
+    .then(e => {
+      let time = [];
+      let count = [];
+      for (let o of e) {
+        time.push(o.time);
+        count.push(o.count);
+      }
+      back.data.time = time;
+      back.data.count = count;
+      back.message = 'success';
+    });
+  ctx.body = back;
+});
 
-module.exports = router
+router.get('/heatMap', async (ctx, next) => {
+  const back = {
+    message: 'fail',
+    data: [],
+  };
+  const floor = ctx.query.floor;
+  if (typeof floor === 'undefined') {
+    ctx.body = back;
+  }
+
+  // await Promise.all(knex('day1_PertimeSid')
+  // .join('sensor', 'day1_PertimeSid.sid', '=', 'sensor.sid')
+  // .where('floor', floor)
+  // .select('x', 'y', 'count'), knex('day1_PertimeSid')
+  // .join('sensor', 'day1_PertimeSid.sid', '=', 'sensor.sid')
+  // .where('floor', floor)
+  // .select('time'))
+  await knex('day1_PertimeSid')
+    .join('sensor', 'day1_PertimeSid.sid', '=', 'sensor.sid')
+    .where('floor', floor)
+    .select('time', 'x', 'y', 'count')
+    .then(e => {
+      if (e.length === 0) {
+        return;
+      }
+      back.message = 'success';
+      e.forEach(item => {
+        const [h, m, _] = item.time.split(':');
+        const index = (+h - 7) * 60 + (+m - 1);
+        if (back.data[index]) {
+          back.data[index].push([item.y, 16 - item.x, item.count]);
+        } else {
+          back.data[index] = [[item.y, 16 - item.x, item.count]];
+        }
+      });
+    });
+  ctx.body = back;
+});
+
+module.exports = router;
