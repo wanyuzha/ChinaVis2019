7060b917743be66d87b76802dfd7afd9ed343ffd
Wan-woo <15056032198@163.com>
2019-06-08 22:49:25 +0800

    增加了时间卡片

---
+++ vis-frontend/src/components/TimeMap.vue
@@ -0,0 +1,17 @@
+<template>
+    <Card style="width:600px">
+        <div style="text-align:center">
+            <h3>A high quality UI Toolkit based on Vue.js</h3>
+        </div>
+    </Card>
+</template>
+<script>
+
+export default {
+    mounted() {
+        this.$bus.$on('timechange', ({time,day}) => {
+
+        })
+    }
+}
+</script>
